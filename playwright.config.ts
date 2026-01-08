da824712a354a2ff8bfc04ffdfde7a5e215ef6d7
cameronindetroit <cameronindetroit@gmail.com>
2026-01-07 18:08:13 -0600

    added environment variables for qa, uat and staging

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,14 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
+
+if (!process.env.NODE_ENV){
+  require('dotenv').config({path:`${__dirname}//src/config//.env`});
+}
+else {
+  require('dotenv').config({path:`${__dirname}//src/config//.env.${process.env.NODE_ENV}`});
+}
+
 // import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,15 +1,21 @@
 import { test } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 
-test("test", async ({ page }) => {
+test.skip("test", async ({ page }) => {
   const loginPage = new LoginPage(page);
   const serviceTitleSelector = "Service";
 
-  await loginPage.navigate();
-    await loginPage.fillUsername("cameronindetroit153@agentforce.com");
-    await loginPage.fillPassword("Viola1987!");
-
+    await loginPage.navigate();;
+    await loginPage.fillUsername(process.env.userid!);
+    await loginPage.fillPassword(process.env.password!)
     const homePage = await loginPage.clickLoginButton();
     // await homePage.expectServiceTitleToBeVisible();
     await page.getByTitle(serviceTitleSelector).waitFor({ state: "visible", timeout: 19000 });
     })
+
+
+    test("Sample env test", async ({ page }) => {
+      console.log("Enviornment: ", process.env.NODE_ENV);
+      console.log("USERNAME: ", process.env.userid);
+      console.log("PASSWORD: ", process.env.password);
+    }); 
