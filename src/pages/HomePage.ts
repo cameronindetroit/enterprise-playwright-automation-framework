7401fd393337810afe68184adaf4a4f800c2dc90
cameronindetroit <cameronindetroit@gmail.com>
2026-01-08 17:14:51 -0600

    added test logging using the winston library

--- src/pages/HomePage.ts
+++ src/pages/HomePage.ts
@@ -1,4 +1,5 @@
 import { Page, expect } from "@playwright/test";
+import logger from "../utils/LoggerUtil";
 
 export default class HomePage {
 
@@ -11,7 +12,11 @@ export default class HomePage {
 
     async expectServiceTitleToBeVisible() {
         await this.page.waitForLoadState("load");
-        await expect(this.page.getByTitle(this.serviceTitleSelector)).toBeVisible({ timeout: 19000 });
+        await expect(this.page.getByTitle(this.serviceTitleSelector)).toBeVisible({ timeout: 19000, }).catch((error) => {
+            logger.error(`Error clicking login button: ${error}`);
+            throw error; // Re-throw the error after logging it
+        }).then(() => logger.info("clicked login button")); {
     }
+  }
 
 }
--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,26 +1,32 @@
-import { test } from "@playwright/test";
+import { test,expect } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 import { encrypt, decrypt } from "../utils/CryptojsUtil";
 import { decryptEnvFile, encryptEnvFile } from "../utils/EncryptEnvFile";
+import logger from "../utils/LoggerUtil";
 
 test("test", async ({ page }) => {
   const loginPage = new LoginPage(page);
-  const serviceTitleSelector = "Service";
+  const serviceTitleSelector = page.getByRole("heading", { name: "Service" });
+  const loginImage = page.locator("#logo");
 
-    await loginPage.navigate();;
+    await loginPage.navigate();
+    await expect(loginImage).toBeVisible({ timeout: 19000 });
     await loginPage.fillUsername(decrypt(process.env.userid!));
     await loginPage.fillPassword(decrypt(process.env.password!));
     const homePage = await loginPage.clickLoginButton();
+    // await expect(serviceTitleSelector).toBeVisible({ timeout: 19000 });
+    logger.info("Test for login is completed");
     // await homePage.expectServiceTitleToBeVisible();
-    await page.getByTitle("Service").waitFor({ state: "visible", timeout: 19000 });
-    })
+    // await page.getByTitle("Service").waitFor({ state: "visible", timeout: 19000 });
+  
+});
 
-    test("Sample env test", async ({ page }) => {
-// const plainText = "Hello, Mars!";
-// const encryptedText = encrypt(plainText);
-// console.log('SALT:', process.env.SALT);
-// console.log('Encrypted Text:', encryptedText);
-// const decryptedText = decrypt(encryptedText);
-// console.log('Decrypted Text:', decryptedText);
-encryptEnvFile();
-}); 
+test.skip("Sample env test", async ({ page }) => {
+  // const plainText = "Hello, Mars!";
+  // const encryptedText = encrypt(plainText);
+  // console.log('SALT:', process.env.SALT);
+  // console.log('Encrypted Text:', encryptedText);
+  // const decryptedText = decrypt(encryptedText);
+  // console.log('Decrypted Text:', decryptedText);
+  encryptEnvFile();
+});
